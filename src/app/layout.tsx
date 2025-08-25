ec9323016eee026fded6fe10b70b7a2a93359190
dany-boop <dny77853@gmail.com>
2025-08-25 17:06:30 +0700

    fix error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Poppins } from 'next/font/google';
 import './globals.css';
 import IntroAnimation from './components/molecules/intro';
-
-export const poppinsLight = Poppins({
-  subsets: ['latin'],
-  weight: '300',
-  variable: '--font-poppins-light',
-});
-
-export const poppinsRegular = Poppins({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-poppins-regular',
-});
-
-export const poppinsBold = Poppins({
-  subsets: ['latin'],
-  weight: '700',
-  variable: '--font-poppins-bold',
-});
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { geistMono, poppinsRegular } from './fonts';
 
 export const metadata: Metadata = {
   title: 'Clixure Digital',
